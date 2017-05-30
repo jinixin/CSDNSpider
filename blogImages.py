95fb69e202ce931db2abf17f34ccbc939e424b9b
jinixin <jixinmanage@qq.com>
2017-05-31 02:01:21 +0800

    增加图像显示功能（测试阶段）
    Signed-off-by: jinixin <jixinmanage@qq.com>

---
+++ blogImages.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+import matplotlib.pyplot as pyplot
+from sqltool import SqlTool
+
+
+class BlogImage(object):
+    def __init__(self):
+        pass
+
+    @classmethod
+    def everyday_view_num(cls):
+        with SqlTool(db='blog_csdn') as cursor:
+            cursor.execute('select sum(number) from read_number group by record_time order by record_time desc limit 30')
+            view_num = cursor.fetchall()  # get the sum of view
+            cursor.execute('select record_time from read_number group by record_time order by record_time desc limit 30')
+            view_date = cursor.fetchall()  # get date
+        pyplot.plot(view_date, view_num)  # x, y
+        pyplot.show()
+
+
+if __name__ == '__main__':
+    BlogImage.everyday_view_num()
